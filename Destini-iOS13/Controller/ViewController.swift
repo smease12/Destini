3babb9ab0a214ab5487a09ed05e2c6382ebe23eb
smease12 <scmease93@gmail.com>
2024-01-15 16:48:03 -0500

    Change story label on viewDidLoad

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,9 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = story0
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
